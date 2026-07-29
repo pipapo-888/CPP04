6e25f4a2991e62ded87986aedb8f1f7313c54d46
pipapo888 <kazuya2002uk@gmail.com>
2026-07-29 20:52:02 +0900

    dog 追加

--- ex00/Dog.cpp
+++ ex00/Dog.cpp
@@ -6,8 +6,37 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 17:41:30 by knomura           #+#    #+#             */
-/*   Updated: 2026/07/29 17:41:36 by knomura          ###   ########.fr       */
+/*   Updated: 2026/07/29 20:51:50 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Dog.hpp"
+
+Dog::Dog() : Animal()
+{
+	_type = "Dog";
+	std::cout << "Dog Default Constructor Called\n";
+}
+
+Dog::Dog(const Dog &obj)
+{
+	_type = obj._type;
+	std::cout << "Dog Copy Constructor Called\n";
+}
+
+Dog &Dog::operator=(const Dog &obj)
+{
+	_type = obj._type;
+	return *this;
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog Destructor Called\n";
+}
+
+void Dog::makeSound()
+{
+	std::cout << _type << " shout Meooow\n";
+}
--- ex00/Dog.hpp
+++ ex00/Dog.hpp
@@ -6,13 +6,25 @@
 /*   By: knomura <knomura@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/29 16:50:34 by knomura           #+#    #+#             */
-/*   Updated: 2026/07/29 19:19:58 by knomura          ###   ########.fr       */
+/*   Updated: 2026/07/29 20:50:46 by knomura          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DOG_HPP
 #define DOG_HPP
 
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+	public:
+		Dog();
+		Dog(const Dog &obj);
+		Dog &operator=(const Dog &obj);
+		~Dog();
+
+		void makeSound();
+};
 
 
 #endif
